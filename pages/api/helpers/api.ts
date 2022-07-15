00923f7ed538ec9ae78684a495a17189af1efb1a
= <=>
2022-07-14 22:00:09 -0500

    Cors

--- pages/api/helpers/api.ts
+++ pages/api/helpers/api.ts
@@ -5,19 +5,20 @@ export const getAllProducts = async () => {
     const promises: Promise<any>[] = [];
     for (let i = 1; i <= 3; i++) {
         promises.push(
-            (await fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST }/inventory/${i}`)).json()
+            (await fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST }/inventory/${i}`, { mode: 'no-cors' })).json()
         );
     }
     return promises;
 }
 
 export const getProducts = async ( page = 1, search = "") => {
-    return await fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST }/inventory?page=${page}&search=${search}`);
+    return await fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST }/inventory?page=${page}&search=${search}`, {mode: 'no-cors'});
 }
 
 export const updateProduct = (product: any, id:any) => {
     return fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST}/products/${id}`, {
         method: 'PUT',
+        mode: 'no-cors',
         body: product
     });
 }
@@ -26,6 +27,7 @@ export const createProduct = (product: any) => {
 
     return fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST}/products/`, {
         method: 'POST',
+        mode: 'no-cors',
         body: product
     });
 }
@@ -40,6 +42,7 @@ export const createVariation = (product: any, parentId: string) => {
 export const updateVariation = (parentId:any, product: any) => {
     return fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST}/products/${parentId}/variation/${product.id}`, {
         method: 'PUT',
+        mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
             //cors
@@ -69,6 +72,7 @@ export const deleteTag = (id:any) => {
 export const createTag = (tag:any) => {
     return fetch(`${process.env.API_HOST ? process.env.API_HOST : ROUTES.API_HOST}/tag/`, {
         method: 'POST',
+        mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
             //cors
